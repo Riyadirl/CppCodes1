2e1bd8f14ca1e0148a6fc1e716b3da054d714cdf
Mahbub Hasaan Riyad <95384246+Riyadirl@users.noreply.github.com>
2022-04-16 21:02:14 +0600

    Add files via upload

---
+++ day1code.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+//#include <conio.h>
+using namespace std;
+int main(){
+
+  cout<<"This is my cpp codes..\n";
+  cout<<"mahbub \nHasan \nRiyad";
+  cout<<"Mahbub"<<endl<< "Cumilla" << endl<< "Bangladesh";
+    return 0;
+}
